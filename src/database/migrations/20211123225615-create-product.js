7b788800008d415f49039688dc96f2ba0827e8ac
Yamila <yamila.bulich@gmail.com git config --global user.email yamila.bulich@gmail.com git config --global user.email yamila.bulich@gmail.com>
2021-11-23 21:03:04 -0300

    first commit

---
+++ src/database/migrations/20211123225615-create-product.js
@@ -0,0 +1,43 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Products', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name: {
+        type: Sequelize.STRING
+      },
+      description: {
+        type: Sequelize.STRING
+      },
+      price: {
+        type: Sequelize.DECIMAL
+      },
+      stock: {
+        type: Sequelize.INTEGER
+      },
+      categoryId: {
+        type: Sequelize.INTEGER,
+        references:{
+          model:'Categories',
+          key:'id'
+        }
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Products');
+  }
+};
---
+++ src/database/models/product.js
@@ -0,0 +1,29 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Product extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      Product.hasMany(models.Category, {foreignKey:'categoryId'});
+      Product.belongsTo(models.Image);
+      Product.belongsTo(models.Review);
+    }
+  };
+  Product.init({
+    name: DataTypes.STRING,
+    description: DataTypes.STRING,
+    price: DataTypes.DECIMAL,
+    stock: DataTypes.INTEGER,
+    categoryId: DataTypes.INTEGER
+  }, {
+    sequelize,
+    modelName: 'Products',
+  });
+  return Product;
+};
